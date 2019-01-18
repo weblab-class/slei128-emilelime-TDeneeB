c98afe2a55fa88b25682a418334e9625d3fb7fcb
Shirley Lei <syl573@nyu.edu>
2019-01-18 08:07:35 -0500

    added boilerplate server code

--- server/app.js
+++ server/app.js
@@ -1,3 +1,7 @@
+//COPIED FROM CATBOOK, EDIT
+//COPIED FROM CATBOOK, EDIT
+//COPIED FROM CATBOOK, EDIT
+
 const express = require("express");
 const path = require("path");
 
@@ -14,4 +18,11 @@ http.listen(3000, () => {
   console.log(`Listening on port 3000 and looking in folder ${publicPath}`);
 });
 
-//emily commented
+// why are we using http.listen(3000) not app.listen(3000) like below?
+
+// app.get(‘/api’, (req, res) => {
+//   res.json({message: ‘Welcome to the Server’});
+// });
+// app.listen(8081, ()=>{
+//   console.log(‘API listening on port 8081’);
+// });
